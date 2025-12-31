254631ffbc5ed7353e8f0fda36e1370fc5008b4d
Kelson Woods <kelson@llamachant.com>
2025-12-30 20:27:26 -0500

    Test

--- GitHubSecuritySample2/Program.cs
+++ GitHubSecuritySample2/Program.cs
@@ -2,33 +2,33 @@
 using System.Security.Cryptography;
 using System.Text;
 
-
 class Program
 {
-    private static string username = "admin";
-    private static string password = "abc123";
     private static string _apiKey = "sk_live_abc123secret";
 
     static void Main(string[] args)
     {
+        Console.Write("Enter username: ");
+        string username = Console.ReadLine();  // User-controlled input
+
+        Console.Write("Enter password: ");
+        string password = Console.ReadLine();  // User-controlled input
+
         SearchUser(username, password);
         HashData("test");
     }
 
     static void SearchUser(string username, string pw)
     {
-        // BAD: SQL injection vulnerability
         string connectionString = "Server=localhost;Database=test;";
         string query = "SELECT * FROM Users WHERE Username = '" + username + "' and Password = '" + pw + "'";
 
         using var connection = new SqlConnection(connectionString);
         using var command = new SqlCommand(query, connection);
-        // This would execute the unsafe query
     }
 
     static byte[] HashData(string input)
     {
-        // BAD: Weak cryptographic algorithm
         using var md5 = MD5.Create();
         return md5.ComputeHash(Encoding.UTF8.GetBytes(input));
     }
