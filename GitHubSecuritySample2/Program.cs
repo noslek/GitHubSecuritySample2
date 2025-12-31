0d4085e3538245fa5b93373b82586def1e39d750
Kelson Woods <kelson@llamachant.com>
2025-12-30 20:06:38 -0500

    Add project files.

---
+++ GitHubSecuritySample2/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Security.Cryptography;
+using System.Text;
+
+
+class Program
+{
+    private static string _apiKey = "sk_live_abc123secret";
+
+    static void Main(string[] args)
+    {
+        Console.Write("Enter username: ");
+        string username = Console.ReadLine();
+
+        SearchUser(username);
+        HashData("test");
+    }
+
+    static void SearchUser(string username)
+    {
+        // BAD: SQL injection vulnerability
+        string connectionString = "Server=localhost;Database=test;";
+        string query = "SELECT * FROM Users WHERE Username = '" + username + "'";
+
+        using var connection = new SqlConnection(connectionString);
+        using var command = new SqlCommand(query, connection);
+        // This would execute the unsafe query
+    }
+
+    static byte[] HashData(string input)
+    {
+        // BAD: Weak cryptographic algorithm
+        using var md5 = MD5.Create();
+        return md5.ComputeHash(Encoding.UTF8.GetBytes(input));
+    }
+}
